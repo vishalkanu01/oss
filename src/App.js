45fa2523304489fafa60c844415638bcfa28788c
vishalkanu01 <vishalkanu01@gmail.com>
2022-06-13 17:15:38 +0545

    working on sidebar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      < Feed />
+      {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Feed() {
+    return (
+        <div>
+            <h2>home</h2>
+            {/* Header */}
+            {/* Tweet box */}
+            {/* Post */}
+        </div>
+    );
+}
+
+export default Feed;
